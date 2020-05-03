6777f9de9e465948899950deb0a333d17253452a
Sam Delay <samuel.delay@gmail.com>
2020-05-02 23:22:47 -0400

    adding checkboxes

--- my-app/src/components/SearchBar.js
+++ my-app/src/components/SearchBar.js
@@ -1,31 +1,40 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container'
+import Col from "react-bootstrap/Col";
 
 const SearchBar = ({ search, handleInputChange, handleFormSubmit }) => {
 
   return (
-    <Container>
-        <Form>
-          <Form.Group>
-            <Form.Label>Directory Search</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Search something..."
-              value={search}
-              onChange={handleInputChange}
-              name="breed"
-            />
-          </Form.Group>
-          <Button
-            variant="primary"
-            onClick={handleFormSubmit}
-          >
-            Search
+    <Form>
+      <Form.Group>
+        <Form.Label>Directory Search</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Search something..."
+          value={search}
+          onChange={handleInputChange}
+          name="search"
+        />
+      </Form.Group>
+      <Form.Group>
+        {['checkbox'].map((type) => (
+          <div key={`inline-${type}`} className="mb-3">
+            <Form.Check inline label="Manager" type={type} id={`inline-${type}-Manager`} />
+            <Form.Check inline label="Employee" type={type} id={`inline-${type}-Employee`} />
+            <Form.Check inline label="All" type={type} id={`inline-${type}-All`} defaultChecked/>
+          </div>
+        ))}
+      </Form.Group>
+      <Button
+        size="lg"
+        variant="outline-secondary"
+        onClick={handleFormSubmit}
+        block
+      >
+        Search
       </Button>
-        </Form>
-    </Container>
+    </Form>
   )
 
 }
