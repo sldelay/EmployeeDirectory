ba496b658e49cbc46cc54a6cfe881a198c4288b2
Sam Delay <samuel.delay@gmail.com>
2020-05-02 23:22:39 -0400

    minor edits

--- my-app/src/components/Jumbotron.js
+++ my-app/src/components/Jumbotron.js
@@ -5,8 +5,7 @@ const Jumbo = () => {
 
   return (
     <Jumbotron className="text-center">
-      <h1>Hello!</h1>
-      <h3>Welcome to your employee directory</h3>
+      <h2>Welcome to your employee directory</h2>
       <p>You can search for your cowrokers contact info by name, phone, or email.</p>
     </Jumbotron>
   )
