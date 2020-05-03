67dbf3ddff0f512273081ad2d9d502a3daaac05d
Sam Delay <samuel.delay@gmail.com>
2020-05-02 21:18:58 -0400

    state, search input, navbar, jumbo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import NavbarComp from "./components/Navbar"
+import Jumbo from "./components/Jumbotron"
+import SearchBar from "./components/SearchBar"
+import employees from "./employees.json"
 
-function App() {
+
+class App extends Component {
+
+  state = {
+    search: "",
+    results: [employees]
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+
+    alert(`You have searched for ${this.state.search}`);
+    this.setState({
+      search: ""
+    });
+  }; 
+
+ render () {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <>
+    <NavbarComp />
+    <Jumbo />
+    <SearchBar 
+    handleInputChange={this.handleInputChange}
+    handleFormSubmit={this.handleFormSubmit}
+    />
+    </>
   );
+ }
 }
 
 export default App;
