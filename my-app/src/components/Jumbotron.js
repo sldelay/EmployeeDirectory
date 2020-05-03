510fcb631b88a4f52861cfd3462da384c24e9f54
Sam Delay <samuel.delay@gmail.com>
2020-05-02 21:19:35 -0400

    welcome jumbotron

---
+++ my-app/src/components/Jumbotron.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+
+const Jumbo = () => {
+
+  return (
+    <Jumbotron className="text-center">
+      <h1>Hello!</h1>
+      <h3>Welcome to your employee directory</h3>
+      <p>You can search for your cowrokers contact info by name, phone, or email.</p>
+    </Jumbotron>
+  )
+
+}
+
+export default Jumbo
