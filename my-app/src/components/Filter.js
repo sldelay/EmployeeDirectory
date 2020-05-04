5bc7699d407076051996d9590c28e44897531af2
Sam Delay <samuel.delay@gmail.com>
2020-05-04 10:44:36 -0400

    className

--- my-app/src/components/Filter.js
+++ my-app/src/components/Filter.js
@@ -9,14 +9,15 @@ const FilterSearch = ({ filter, handleFilterChange, resetFilter }) => {
       <Form.Group>
         {['radio'].map((type) => (
           <div key={`inline-${type}`} className="mb-3" >
-            <Form.Check inline label="Manager" type={type} id={`inline-${type}-Manager`} name="filter" value="manager" onChange={handleFilterChange}/>
-            <Form.Check inline label="Employee" type={type} id={`inline-${type}-Employee`} name="filter" value="employee" onChange={handleFilterChange}/>
-            <Form.Check inline label="All" type={type} id={`inline-${type}-All`} name="filter" value="all" defaultChecked onChange={handleFilterChange}/>
+            <Form.Check inline label="Manager" type={type} id={`inline-${type}-Manager`} name="filter" value="manager" onChange={handleFilterChange} />
+            <Form.Check inline label="Employee" type={type} id={`inline-${type}-Employee`} name="filter" value="employee" onChange={handleFilterChange} />
+            <Form.Check inline label="All" type={type} id={`inline-${type}-All`} name="filter" value="all" defaultChecked onChange={handleFilterChange} />
           </div>
         ))}
       </Form.Group>
       {filter !== "all" && (
         <Button
+          className="filterBtn"
           size="lg"
           type="submit"
           variant="outline-secondary"
