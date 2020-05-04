7d18fa946c9538ab6c4864ad4c16cab92dcf6b81
Sam Delay <samuel.delay@gmail.com>
2020-05-04 10:43:28 -0400

    changing card row col

--- my-app/src/components/EmployeeCard.js
+++ my-app/src/components/EmployeeCard.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
+import Col from "react-bootstrap/Col";
 
 
 const EmployeeCard = ({ data }) => {
 
-  const { name, phone, email} = data
+  const { name, phone, email } = data
 
   return (
-    <Card className="employeeCard">
-      <Card.Body>
-        <h3>{name}</h3>
-        <p>{`Phone: ${phone}`}</p>
-        <p>{`Email: ${email}`}</p>
-      </Card.Body>
-    </Card>
+    <Col lg={6}>
+      <Card className="employeeCard">
+        <Card.Body>
+          <h3>{name}</h3>
+          <p>{`Phone: ${phone}`}</p>
+          <p>{`Email: ${email}`}</p>
+        </Card.Body>
+      </Card>
+    </Col>
   )
 
 }
