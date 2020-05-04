58455e2bc5bd910879c3d4350d581a451e318f38
Sam Delay <samuel.delay@gmail.com>
2020-05-03 23:49:15 -0400

    filter functionality

--- my-app/src/components/EmployeeDisplay.js
+++ my-app/src/components/EmployeeDisplay.js
@@ -5,13 +5,33 @@ import EmployeeCard from './EmployeeCard';
 
 
 
-const EmployeeDisplay = ({ employees }) => {
+const EmployeeDisplay = ({ employees, search, filter }) => {
+
+  // eslint-disable-next-line
+  const empList = employees.filter((data) => {
+    if (search === "") {
+      return data;
+    } else if (data.name.toLowerCase().includes(search.toLowerCase()) ||
+      data.email.toLowerCase().includes(search.toLowerCase()) ||
+      data.phone.toLowerCase().includes(search.toLowerCase())) {
+      return data;
+    }
+  })
+
+  // eslint-disable-next-line
+  const filterList = empList.filter((data) => {
+    if (filter === "all") {
+      return data;
+    } else if (data.role.toLowerCase().includes(filter.toLowerCase())) {
+      return data;
+    }
+  })
 
   return (
     <CardColumn>
-    {employees.map(employee => (
-      <EmployeeCard key={employee.id} data={employee}/>
-    ))}
+      {filterList.map(employee => (
+        <EmployeeCard key={employee.id} data={employee} />
+      ))}
     </CardColumn>
   )
 
