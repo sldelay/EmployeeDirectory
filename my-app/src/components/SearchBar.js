8901dcf938d6a3aa5d982140df5a2308bdbb70a5
Sam Delay <samuel.delay@gmail.com>
2020-05-03 23:50:13 -0400

    moved radio buttons, added clear btn

--- my-app/src/components/SearchBar.js
+++ my-app/src/components/SearchBar.js
@@ -1,40 +1,36 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import Col from "react-bootstrap/Col";
 
-const SearchBar = ({ search, handleInputChange, handleFormSubmit }) => {
+const SearchBar = ({ search, handleInputChange, clearSearch }) => {
 
   return (
-    <Form>
-      <Form.Group>
-        <Form.Label>Directory Search</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Search something..."
-          value={search}
-          onChange={handleInputChange}
-          name="search"
-        />
-      </Form.Group>
-      <Form.Group>
-        {['checkbox'].map((type) => (
-          <div key={`inline-${type}`} className="mb-3">
-            <Form.Check inline label="Manager" type={type} id={`inline-${type}-Manager`} />
-            <Form.Check inline label="Employee" type={type} id={`inline-${type}-Employee`} />
-            <Form.Check inline label="All" type={type} id={`inline-${type}-All`} defaultChecked/>
-          </div>
-        ))}
-      </Form.Group>
-      <Button
-        size="lg"
-        variant="outline-secondary"
-        onClick={handleFormSubmit}
-        block
-      >
-        Search
-      </Button>
-    </Form>
+
+      <Form>
+        <Form.Group>
+          <Form.Label><h4>Directory Search</h4></Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Search something..."
+            value={search}
+            onChange={handleInputChange}
+            name="search"
+          />
+        </Form.Group>
+        {search.length > 0 && (
+          <Button
+            className="clearBtn"
+            size="lg"
+            type="click"
+            variant="outline-secondary"
+            onClick={clearSearch}
+            block
+          >
+            Clear Search
+          </Button>
+        )}
+      </Form>
+
   )
 
 }
